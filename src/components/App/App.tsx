bbfbdaf19b64a9d79407ca62344113293d95c756
Jan-Otto Kröpke <github@jkroepke.de>
2024-09-15 21:27:29 +0200

    Update App.tsx
    Signed-off-by: Jan-Otto Kröpke <github@jkroepke.de>

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import { AppRootProps } from '@grafana/data';
 import { PluginPropsContext } from '../../utils/utils.plugin';
-import { Routes } from '../Routes';
+import { Status } from '../../pages/Status';
 
 export class App extends React.PureComponent<AppRootProps> {
   render() {
     return (
       <PluginPropsContext.Provider value={this.props}>
-        <Routes />
+        <Routes>
+          {/* Default page */}
+          <Route path="*" element={<Status />} />
+        </Routes>
       </PluginPropsContext.Provider>
     );
   }
