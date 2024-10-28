3c0efd26e65aa7be5c958ee53875f89de93590c5
Jan-Otto Kröpke <joe@cloudeteer.de>
2024-10-29 00:03:08 +0100

    remove router dependency
    Signed-off-by: Jan-Otto Kröpke <joe@cloudeteer.de>

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import { AppRootProps } from '@grafana/data';
 import { PluginPropsContext } from '../../utils/utils.plugin';
-import { Routes } from '../Routes';
+import { Status } from "../../pages/Status";
 
 export class App extends React.PureComponent<AppRootProps> {
   render() {
     return (
       <PluginPropsContext.Provider value={this.props}>
-        <Routes />
+        <Status />
       </PluginPropsContext.Provider>
     );
   }
